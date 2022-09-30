f1cddbfbfa2477beded8eef8b5d964257fe7d037
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-09-30 11:56:16 +0100

    * generalised to any age window size * backfill with nsvt = 0

--- scripts/twofold.R
+++ scripts/twofold.R
@@ -1,63 +1,72 @@
 # https://ignacioriveros1.github.io/r/2020/03/22/r_and_stata.html
 
 library(RStata)
+library(dplyr)
+library(reshape2)
+
 
 options("RStata.StataPath" = "\"C:\\Program Files\\Stata17\\StataMP-64\"")
 options("RStata.StataVersion" = 17)
 
 load("~/R/hcmmice/data/data_set1.RData")
 
-# rearrange data to wide format
-dat1 <-
-  data_set1 |>
-  filter(!is.na(male)) |>
-  select(id, age, nsvt, syncope, male) |>
-  mutate(age1 = floor(age),
-         age2 = age1 + 1,
-         age3 = age1 + 2,
-         firstyear = 1,
-         lastyear = 3,
-         nsvt1 = nsvt,
-         nsvt2 = NA,
-         nsvt3 = NA) |>
-  select(-age, -nsvt)
-
-# export data frame to Stata binary format
-foreign::write.dta(dat1, "data/widedata.dta")
-
-
-#############
-# imputation
-
-twofold_out <-
-  stata("scripts/twofold.do",
-        data.in = dat1, data.out = TRUE) |>
-  as_tibble()
-
-twofold_out
+# # rearrange data to wide format
+# dat1 <-
+#   data_set1 |>
+#   filter(!is.na(male)) |>
+#   select(id, age, nsvt, syncope, male) |>
+#   mutate(age1 = floor(age),
+#          age2 = age1 + 1,
+#          age3 = age1 + 2,
+#          firstyear = 1,
+#          lastyear = 3,
+#          nsvt1 = nsvt,
+#          nsvt2 = NA,
+#          nsvt3 = NA) |>
+#   select(-age, -nsvt)
+#
+# # export data frame to Stata binary format
+# foreign::write.dta(dat1, "data/widedata.dta")
+#
+#
+# #############
+# # imputation
+#
+# twofold_out <-
+#   stata("scripts/twofold.do",
+#         data.in = dat1, data.out = TRUE) |>
+#   as_tibble()
+#
+# twofold_out
 
 
 
 # -------------------------------------------------------------------------
 
-library(reshape2)
+FIRSTYEAR <- 41
+LASTYEAR <- 50
+
+range_years <- FIRSTYEAR:LASTYEAR
+n_years <- length(range_years)
 
 dat45 <-
   data_set1 |>
   select(id, age, nsvt, syncope, male, mwt, fhxscd, lvedd, fs) |>
   mutate(age = floor(age)) |>
-  filter(age > 40, age <= 45)
+  filter(age >= FIRSTYEAR, age <= LASTYEAR)
 
 dat2 <-
-  cbind.data.frame(id = rep(dat45$id, each = 5),
-        age = rep(41:45, nrow(dat45))) |>
+  cbind.data.frame(
+    id = rep(dat45$id, each = n_years),
+    age = rep(range_years, nrow(dat45))) |>
   full_join(dat45, by = c("id", "age")) |>
-  dcast(id~age, value.var = "nsvt") |>
-  rename_with(~paste0("nsvt", .x), !starts_with("id")) |>
-  mutate(firstyear = 41,
-         lastyear = 45) |>
+  dcast(id ~ age, value.var = "nsvt") |>
+  rename_with(~ paste0("nsvt", .x), !starts_with("id")) |>
+  mutate(firstyear = FIRSTYEAR,
+         lastyear = LASTYEAR) |>
   full_join(dat45, by = "id") |>
-  select(-nsvt, -syncope)
+  select(-nsvt, -syncope) |>
+  mutate(firstyear = age)  # left-censoring
 
 
 twofold_out2 <-
@@ -69,8 +78,52 @@ twofold_out2
 
 twofold_out2 <-
   twofold_out2 |>
-  mutate(nsvt41 = as.numeric(nsvt41 >= 0.5),
-         nsvt42 = as.numeric(nsvt42 >= 0.5),
-         nsvt43 = as.numeric(nsvt43 >= 0.5),
-         nsvt44 = as.numeric(nsvt44 >= 0.5),
-         nsvt45 = as.numeric(nsvt45 >= 0.5))
+  mutate(across(starts_with("nsvt"), ~ as.numeric(.x >= 0.5)))
+
+
+
+# deterministically fill in some nsvt ------------------------------
+
+dat45_obs <-
+  dat45 |>
+  mutate(age_obs = age,
+         nsvt_obs = nsvt) |>
+  select(id, age_obs, nsvt_obs)
+
+dat3 <-
+  cbind.data.frame(
+    id = rep(dat45$id, each = n_years),
+    age = rep(range_years, nrow(dat45))) |>
+  full_join(dat45, by = c("id", "age")) |>
+  full_join(dat45_obs, by = "id") |>
+  group_by(id) |>
+  # left fill nsvt == 0
+  # right fill nsvt == 1
+  mutate(nsvt = ifelse(age_obs > age & nsvt_obs == 0, 0, nsvt)#,
+         # nsvt = ifelse(age_obs < age & nsvt_obs == 1, 1, nsvt)
+         ) |>
+  ungroup() |>
+  select(-age_obs, -nsvt_obs) |>
+  dcast(id ~ age, value.var = "nsvt") |>
+  rename_with(~ paste0("nsvt", .x), !starts_with("id")) |>
+  mutate(firstyear = FIRSTYEAR,
+         lastyear = LASTYEAR) |>
+  full_join(dat45, by = "id") |>
+  select(-nsvt, -syncope) |>
+  mutate(firstyear = age)  # left-censoring
+
+
+twofold_out3 <-
+  stata("scripts/twofold2.do",
+        data.in = dat3, data.out = TRUE) |>
+  as_tibble()
+
+twofold_out3
+
+twofold_out3 <-
+  twofold_out3 |>
+  mutate(across(starts_with("nsvt"), ~ as.numeric(.x >= 0.5)))
+
+
+
+
--- scripts/twofold2.do
+++ scripts/twofold2.do
@@ -1,1 +1,1 @@
-twofold, timein(firstyear) timeout(lastyear) clear depmis(nsvt) indobs(age male mwt fhxscd lvedd fs) cat(age male) m(2) ba(1) bw(1) width(3) im table
+twofold, timein(firstyear) timeout(lastyear) clear depmis(nsvt) indobs(age male mwt fhxscd lvedd fs) cat(age male) m(5) ba(1) bw(1) width(1) im table
